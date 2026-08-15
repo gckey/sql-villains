bbd66fffcff52ee8c6dbc9435830a50a1d143a14
gckey <abdorehman@gmail.com>
2026-08-15 15:37:17 -0600

    Add initial data into the database

---
+++ db/seeds/01_movie_villains.sql
@@ -0,0 +1,6 @@
+INSERT INTO movie_villains(name, movie) VALUES
+('Darth Vader', 'Star Wars'),
+('Joker', 'The Dark Knight'),
+('Hannibal Lecter', 'The Silence of the Lambs'),
+('Nurse Ratched', 'One Flew Over the Cuckoo Nest'),
+('Terminator', 'The Terminator');
