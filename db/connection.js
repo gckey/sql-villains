bbf724a608a2e01ae9e2fd08350dfc96eb11ee8d
gckey <abdorehman@gmail.com>
2026-08-19 16:19:51 -0600

    Correct DB.PORT to DB_PORT in connection config

--- db/connection.js
+++ db/connection.js
@@ -12,7 +12,7 @@ const Client = pg.Client;
 */
 const config = {
   host:     process.env.DB_HOST,
-  port:     process.env.DB.PORT,
+  port:     process.env.DB_PORT,
   database: process.env.DB_NAME,
   user:     process.env.DB_USER,
   password: process.env.DB_PASS
